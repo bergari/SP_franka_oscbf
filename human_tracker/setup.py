972a3b370f95f659146c6ddc3ce2db90100f0663
bergari <abergmann@ethz.ch>
2026-03-04 18:21:16 +0100

    Integrated human_tracker package into Franka OSCBF repo

---
+++ human_tracker/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'human_tracker'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='pdzw120w',
+    maintainer_email='abergmann@student.ethz.ch',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        'skeleton_tracker_node = human_tracker.skeleton_tracker_node:main'
+        ],
+    },
+)
